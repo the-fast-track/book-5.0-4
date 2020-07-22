e014d6f4c9c8ac632626bfecf8a850af0f722957
Fabien Potencier <fabien@potencier.org>
2020-07-22 17:08:30 +0200

    Add 14.4: Handling a Form

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CommentRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CommentRepository::class)
